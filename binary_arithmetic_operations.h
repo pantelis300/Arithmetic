bd7b6362aa4f661d54896b9812ba60b4840caad2
Pantelis <pantelis300@yahoo.gr>
2018-04-07 19:32:59 +0100

    Add documentation for binary_arithmetic_operations functions

--- binary_arithmetic_operations.h
+++ binary_arithmetic_operations.h
@@ -13,10 +13,42 @@ typedef struct
   char carry_out;
 } full_adder_output_t;
 
-
+/**
+ * Function: full_adder
+ * --------------------
+ * Implements the full-adder's functionality.
+ *
+ * INPUTS
+ *  char a : first operand can only be 0 or 1
+ *  char b : second operand can only be 0 or 1
+ *  char carry_in: carry-in can only be 0 or 1
+ * OUTPUT
+ *  full_adder_output_t struct containing the two ouputs:
+ *  char sum and char carry_out
+ */
 full_adder_output_t full_adder(char a, char b, char carry_in);
 
-
+/**
+ * Function: ripple_binary_addition
+ * --------------------------------
+ * Implements the ripple-carry adder's functionality.
+ *
+ * INPUTS
+ *  char *first_operand : a null terminated string containing
+ *                        the binary representation of the
+ *                        first operand with operand_len-bits
+ *                        All characters must be '0' or '1'
+ *  char *second_operand : a null terminated string containing
+ *                        the binary representation of the
+ *                        second operand with operand_len-bits
+ *                        All characters must be '0' or '1'
+ *  char *carry_out : a reference to the carry_out of the adder
+ *  operand_len : the number of bits (size of strings) 
+ *                of the two operands
+ * RETURNS
+ *  a null terminated string of size operand_len-bits
+ *  representing the sum of the input operands.
+ */
 char *ripple_binary_addition
 (
   const char *first_operand,
@@ -25,7 +57,24 @@ char *ripple_binary_addition
   const size_t operand_len
 );
   
-
+/**
+ * Function: serial_binary_multiplication
+ * --------------------------------------
+ * Implements the radix-2 multiplication.
+ *
+ * INPUTS
+ *  char *first_operand : a null terminated string containing
+ *                        the binary representation of the
+ *                        first operand with operand_len-bits
+ *  char *second_operand : a null terminated string containing
+ *                        the binary representation of the
+ *                        second operand with operand_len-bits
+ *  operand_len : the number of bits (size of strings) 
+ *                of the two operands
+ * RETURNS
+ *  a null terminated string of size (2*operand_len)-bits
+ *  representing the product of the input operands.
+ */
 char *serial_binary_multiplication
 (
   const char *first_operand,
